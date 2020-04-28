c84b4386dd5130c854869fc70a0aa52639805f27
Lee McCormick <lzm0010@gmail.com>
2020-04-28 12:02:10 -0500

    added ticket methods

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,5 +6,18 @@ class Ticket < ApplicationRecord
   validates :bet_id, uniqueness: { scope: :user_id, 
     message: "You have an outstanding bet on this game" }
 
-    
+  def return
+    if self.bet.outcome == "Win"
+      if self.bet.odds > 0
+        return self.amount * (self.bet.odds / 100)
+      else
+        return self.amount / (self.bet.odds / 100)
+      end
+    elsif self.bet.outcome == "Push"
+      return 0
+    else
+      return -self.amount
+    end
+  end
+
 end
